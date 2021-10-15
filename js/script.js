b77b06d512efb39ef60e49e9b4dbfa5c2af637a4
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-15 10:47:35 -0600

    add, subtract, check

--- js/script.js
+++ js/script.js
@@ -10,3 +10,65 @@ active li html
 </li>;
 
 */
+
+/* -------------
+
+  ADD ITEMS
+
+------------- */
+
+const form = document.querySelector("form");
+const activeUL = document.querySelector("#active");
+let remove;
+let items;
+let textarea = document.querySelector("textarea");
+
+textarea.focus();
+
+function addItem(e) {
+  const newItem = document.querySelector("#new-item").value;
+  e.preventDefault();
+  const n = Math.random();
+  activeUL.insertAdjacentHTML(
+    "beforeend",
+    `
+  <li id="li${n}">
+    <label for="${n}" class="todo-container">
+      <input type="checkbox" id="${n}" class="item" />
+      <p>${newItem}</p>
+      <button class="remove" id="r${n}"">X</button>
+    </label>
+  </li>
+  `
+  );
+  const checkbox = document.getElementById(n);
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      checkbox.parentElement.style.background = "darkGray";
+    } else {
+      checkbox.parentElement.style.background = "";
+    }
+  });
+  const removeBtn = document.getElementById(`r${n}`);
+  removeBtn.addEventListener("click", () => {
+    const li = document.getElementById(`li${n}`);
+    li.remove();
+  });
+  // let checkbox = items[i].children[0].children[0];
+}
+
+form.addEventListener("submit", (e) => {
+  addItem(e);
+});
+
+/* -----------------
+
+  CHECKED ITEMS
+
+----------------- */
+
+/* -------------
+
+  REMOVE ITEMS
+
+------------- */
