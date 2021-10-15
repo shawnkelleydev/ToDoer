3395c347b9d01ffc4daa008968cbe4600b2c605a
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-15 08:59:41 -0600

    css, html refactor

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+
+active li html
+
+<li>
+  <label for="item" class="todo-container">
+    <input type="checkbox" id="item" />
+    <span>Make a todo app for personal use</span>
+  </label>
+</li>;
+
+*/
